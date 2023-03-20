974784d013100f630b6c95b29c92ba6690ef5d04
bsu0404 <bsu0404@naver.com>
2023-03-20 22:31:04 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.camera, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.paperplane,
+                color: Colors.black,
+              ))
+        ],
+      ),
+      body: Center(child: Text("hello flutter")),
+    );
+  }
+}
